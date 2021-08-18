b5569071bf7d4f8529528001ecd51c5dd91e2abe
PallagiJoe <pallagijoe@gmail.com>
2021-08-18 16:53:01 +0200

    introduce reducer

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     AppRoutingModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({shoppingListReducer: ShoppingListReducer})
   ],
   bootstrap: [AppComponent],
 })
---
+++ recipe-book/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,23 @@
+
+import { Ingredient } from "src/app/shared/ingredient.model";
+import * as Actions from "./shopping-list.actions";
+const initialState = {
+    ingredients: [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ]
+}
+export function ShoppingListReducer(state = initialState, action: Actions.AddIngredient) {
+    switch (action.type) {
+        case Actions.ADD_INGREDIENT :
+            return {
+                ...state,
+                ingredients: [...state.ingredients, action.payload]
+            };
+
+            console.log(state.ingredients)
+            default: 
+            console.log("asdf")
+            return state;
+    }
+}
